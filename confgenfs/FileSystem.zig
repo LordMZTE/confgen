177991dc19e708b89ae8bef6c76db2760a635abf
LordMZTE <lord@mzte.de>
2024-03-08 12:16:11 +0100

    feat(confgenfs): implement eval filesystem API

--- confgenfs/FileSystem.zig
+++ confgenfs/FileSystem.zig
@@ -35,6 +35,9 @@ const fuse_op_impl = struct {
         // Attributes never change
         cfg.?.attr_timeout = 30;
 
+        // Without this, no attempts to read files with a reported size of 0 will be made.
+        cfg.?.direct_io = 1;
+
         const init_data: *InitData = @alignCast(@ptrCast(c.fuse_get_context().*.private_data));
 
         const fs = init_data.alloc.create(FileSystem) catch |e| {
@@ -71,20 +74,24 @@ const fuse_op_impl = struct {
             return errnoRet(.NFILE);
         };
 
-        if (fi.flags.ACCMODE != .RDONLY) return errnoRet(.ACCES);
+        fs.handles[handle_idx] = if (std.mem.eql(u8, path, "_cgfs/eval")) blk: {
+            if (fi.flags.ACCMODE != .WRONLY) return errnoRet(.ACCES);
+            break :blk .{ .special_eval = .{} };
+        } else blk: {
+            if (fi.flags.ACCMODE != .RDONLY) return errnoRet(.ACCES);
 
-        const cgfile = fs.cg_state.files.get(path) orelse return errnoRet(.NOENT);
+            const cgfile = fs.cg_state.files.get(path) orelse return errnoRet(.NOENT);
 
-        const content = fs.generateCGFile(cgfile, path) catch |e| {
-            std.log.err("generating '{s}': {}", .{ path, e });
-            return errnoRet(.PERM);
+            const content = fs.generateCGFile(cgfile, path) catch |e| {
+                std.log.err("generating '{s}': {}", .{ path, e });
+                return errnoRet(.PERM);
+            };
+
+            std.log.debug("new handle idx: {}", .{handle_idx});
+
+            break :blk .{ .cgfile = content };
         };
 
-        std.log.debug("new handle idx: {}", .{handle_idx});
-
-        const handle = FileHandle{ .cgfile = content };
-
-        fs.handles[handle_idx] = handle;
         fi.fh = handle_idx;
 
         return 0;
@@ -115,7 +122,37 @@ const fuse_op_impl = struct {
                 @memcpy(buf[0..len], cont_off[0..len]);
                 return @intCast(len);
             },
+            .special_eval => return 0,
         }
+    }
+
+    pub fn write(
+        path_p: ?[*:0]const u8,
+        buf_p: ?[*]const u8,
+        bufsiz: usize,
+        offset: c_long,
+        fi_r: ?*c.fuse_file_info,
+    ) callconv(.C) c_int {
+        _ = path_p;
+        const fi: *ffi.fuse_file_info = @alignCast(@ptrCast(fi_r));
+        const fs: *FileSystem = @alignCast(@ptrCast(c.fuse_get_context().*.private_data));
+
+        const buf = buf_p.?[0..bufsiz];
+
+        std.log.debug("{}", .{offset});
+
+        switch (fs.handles[fi.fh] orelse return errnoRet(.BADF)) {
+            .cgfile => return errnoRet(.BADF),
+            .special_eval => |*content| {
+                content.appendSlice(
+                    fs.alloc,
+                    buf,
+                ) catch return errnoRet(.NOMEM);
+                return @intCast(buf.len);
+            },
+        }
+
+        return 0;
     }
 
     pub fn release(path_p: ?[*:0]const u8, fi_r: ?*c.fuse_file_info) callconv(.C) c_int {
@@ -124,9 +161,18 @@ const fuse_op_impl = struct {
         const fs: *FileSystem = @alignCast(@ptrCast(c.fuse_get_context().*.private_data));
 
         if (fs.handles[fi.fh]) |h| {
-            std.log.debug("freed handle idx {}", .{fi.fh});
-            h.deinit(fs.alloc);
-            fs.handles[fi.fh] = null;
+            defer {
+                std.log.debug("freed handle idx {}", .{fi.fh});
+                h.deinit(fs.alloc);
+                fs.handles[fi.fh] = null;
+            }
+
+            switch (h) {
+                .special_eval => |code| {
+                    fs.eval(code.items) catch return errnoRet(.IO);
+                },
+                else => {},
+            }
         } else {
             return errnoRet(.BADF);
         }
@@ -144,7 +190,10 @@ const fuse_op_impl = struct {
         const file_mode = std.os.S.IFREG | 0o444;
 
         const stat: *std.os.Stat = @ptrCast(stat_r.?);
-        stat.* = std.mem.zeroInit(std.os.Stat, .{});
+        stat.* = std.mem.zeroInit(std.os.Stat, .{
+            .uid = std.os.linux.getuid(),
+            .gid = std.os.linux.getgid(),
+        });
 
         const fs: *FileSystem = @alignCast(@ptrCast(c.fuse_get_context().*.private_data));
 
@@ -156,10 +205,16 @@ const fuse_op_impl = struct {
             return 0;
         }
 
+        if (std.mem.eql(u8, path, "_cgfs/eval")) {
+            stat.mode = std.os.S.IFREG | 0o644;
+            stat.nlink = 1;
+            return 0;
+        }
+
         if (fs.cg_state.files.contains(path)) {
             stat.mode = file_mode;
             stat.nlink = 1;
-            stat.size = 1024 * 1024;
+            //stat.size = 1024 * 1024;
         } else if (fs.directory_cache.contains(path)) {
             stat.mode = dir_mode;
             stat.nlink = 2;
@@ -188,6 +243,11 @@ const fuse_op_impl = struct {
         _ = filler.?(buf, "..", null, 0, 0);
 
         const fs: *FileSystem = @alignCast(@ptrCast(c.fuse_get_context().*.private_data));
+
+        if (std.mem.eql(u8, path, "_cgfs")) {
+            _ = filler.?(buf, "eval", null, 0, 0);
+            return 0;
+        }
 
         inline for (.{ fs.directory_cache, fs.cg_state.files }) |map| {
             var iter = map.keyIterator();
@@ -233,10 +293,13 @@ const DirectoryCache = std.HashMap([:0]const u8, void, struct {
 
 const FileHandle = union(enum) {
     cgfile: []const u8,
+    special_eval: std.ArrayListUnmanaged(u8),
 
-    pub fn deinit(self: FileHandle, alloc: std.mem.Allocator) void {
+    pub fn deinit(self_const: FileHandle, alloc: std.mem.Allocator) void {
+        var self = self_const;
         switch (self) {
             .cgfile => |data| alloc.free(data),
+            .special_eval => |*data| data.deinit(alloc),
         }
     }
 };
@@ -290,6 +353,13 @@ fn computeDirectoryCache(self: *FileSystem) !void {
             self.alloc.free(key.*);
         }
         self.directory_cache.clearRetainingCapacity();
+    }
+
+    {
+        const cgfs_p = try self.alloc.dupeZ(u8, "_cgfs");
+        errdefer self.alloc.free(cgfs_p);
+
+        try self.directory_cache.put(cgfs_p, {});
     }
 
     var iter = self.cg_state.files.keyIterator();
@@ -378,4 +448,18 @@ fn generateCGFile(self: *FileSystem, cgf: libcg.luaapi.CgFile, name: []const u8)
     defer tmpl.deinit(self.alloc);
 
     return try libcg.luaapi.generate(self.l, tmpl);
+}
+
+fn eval(self: *FileSystem, code: []const u8) !void {
+    if (libcg.c.luaL_loadbuffer(self.l, code.ptr, code.len, "<cgfs-eval>") != 0) {
+        std.log.err("unable to load eval code: {s}", .{libcg.ffi.luaToString(self.l, -1)});
+        libcg.c.lua_pop(self.l, 1);
+        return error.InvalidEvalCode;
+    }
+
+    if (libcg.c.lua_pcall(self.l, 0, 0, 0) != 0) {
+        std.log.err("unable to run eval code: {s}", .{libcg.ffi.luaToString(self.l, -1)});
+        libcg.c.lua_pop(self.l, 1);
+        return error.InvalidEvalCode;
+    }
 }
