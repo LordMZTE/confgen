6e2f207e2135cedec7fb490b2321fe250d2e6dfb
LordMZTE <lord@mzte.de>
2024-04-25 12:43:05 +0200

    feat: add shell completions

---
+++ share/fish/vendor_completions.d/confgen.fish
@@ -0,0 +1,6 @@
+# Remove previous completions
+complete -c confgen -e
+
+complete -c confgen -s h -l help -d "Show help"
+complete -c confgen -s c -l compile -d "Compile a template to Lua instead of running" -Fr
+complete -c confgen -s j -l json-opt -d "Write the given or all fields from cg.opt to stdout as JSON after running the given confgenfile instead of running" -Fr
---
+++ share/fish/vendor_completions.d/confgenfs.fish
@@ -0,0 +1,4 @@
+# Remove previous completions
+complete -c confgenfs -e
+
+complete -c confgenfs -s h -l help -d "Show help"
