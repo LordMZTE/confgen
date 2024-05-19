1f076bd833b568e8fc522afa4845f1d4db91fd20
LordMZTE <lord@mzte.de>
2024-05-19 23:24:15 +0200

    chore: update docs & completions with new eval parameter

--- share/fish/vendor_completions.d/confgen.fish
+++ share/fish/vendor_completions.d/confgen.fish
@@ -5,3 +5,5 @@ complete -c confgen -s h -l help -d "Show help"
 complete -c confgen -s c -l compile -d "Compile a template to Lua instead of running" -Fr
 complete -c confgen -s j -l json-opt -d "Write the given or all fields from cg.opt to stdout as JSON after running the given confgenfile instead of running" -Fr
 complete -c confgen -s f -l file -d "Evaluate a single template and write the output instead of running"
+complete -c confgen -s e -l eval -r -d "Evaluate the given lua code before loading the confgenfile"
+complete -c confgen -s p -l post-eval -r -d "Evaluate the given lua code after loading the confgenfile"
--- share/fish/vendor_completions.d/confgenfs.fish
+++ share/fish/vendor_completions.d/confgenfs.fish
@@ -2,3 +2,5 @@
 complete -c confgenfs -e
 
 complete -c confgenfs -s h -l help -d "Show help"
+complete -c confgenfs -s e -l eval -r -d "Evaluate the given lua code before loading the confgenfile"
+complete -c confgenfs -s p -l post-eval -r -d "Evaluate the given lua code after loading the confgenfile"
